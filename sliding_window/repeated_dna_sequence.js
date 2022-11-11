cd3d17b6e5420854993142564ded3721fd69c2d2
jjnorris31 <jjnorris31@gmail.com>
2022-11-11 10:09:57 -0600

    added repeated dna sequence solution with navie approach and sliding window approach

--- sliding_window/repeated_dna_sequence.js
+++ sliding_window/repeated_dna_sequence.js
@@ -1,27 +1,52 @@
-function repeatedDNASequence(a1, n1) {
-    let result = new Map();
-    let res = [];
+// Given a string s that represents a DNA sequence, and a number k, return all the contiguous sequences (substrings) of
+// length k that occur more than once in the string. The order of the returned subsequences does not matter. If no repeated
+// subsequence is found, the function should return an empty array.
 
-    if (a1.length < n1) {
-        return [];
-    }
-    for (let i = 0; i < a1.length - n1; i++) {
-        if (result.get(a1.slice(i, i + n1))) {
-            result.set(a1.slice(i, i + n1), result.get(a1.slice(i, i + n1)) + 1);
+// naive approach
+//function findRepeatedSequences(s, l) {
+//    let i = 0;
+//    let k = l + 1;
+//    let result = new Map();
+//
+//    while (i + l <= s.length) {
+//        k = i + l + 1;
+//        const currentWindow = s.slice(i, i + l);
+//        if (k + l > s.length) {
+//            return Array.from(result.keys());
+//        }
+//        while (k + l <= s.length) {
+//            const compareWindow = s.slice(k, k + l);
+//            if (compareWindow === currentWindow) {
+//                if (!result.get(currentWindow)) {
+//                    result.set(currentWindow, 1);
+//                }
+//            }
+//            k++;
+//        }
+//        i++;
+//    }
+//}
+
+// sliding window approach
+function findRepeatedSequences(s, l) {
+    let i = 0;
+    let resultMap = new Map();
+    while (i + l <= s.length) {
+        const currentWindow = s.slice(i, i + l);
+
+        if (resultMap.get(currentWindow)) {
+            resultMap.set(currentWindow, resultMap.get(currentWindow) + 1);
         } else {
-            result.set(a1.slice(i, i + n1), 1);
+            resultMap.set(currentWindow, 1);
         }
+        i++;
     }
 
-    result.forEach((value, key) => {
+    let result = [];
+    resultMap.forEach((value, key) => {
         if (value > 1) {
-            res.push(key);
+            result.push(key);
         }
     });
-
-    return res.length > 0 ? res : a1;
-
+    return result;
 }
-
-
-console.log(repeatedDNASequence("AAAAAAAAAA", 10));
